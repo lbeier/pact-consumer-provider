61b3a7e4cde1d5e24a6354506f78926a0a83282b
Lucas Falk Beier <lucas.beier@gmail.com>
2019-01-10 11:30:59 +0100

    Removing those useless ;

--- consumer/publishToBrocker.js
+++ consumer/publishToBrocker.js
@@ -1,5 +1,5 @@
-let publisher = require('@pact-foundation/pact-node');
-let path = require('path');
+let publisher = require('@pact-foundation/pact-node')
+let path = require('path')
 
 let opts = {
   providerBaseUrl: 'http://127.0.0.1:8989',
@@ -7,7 +7,7 @@ let opts = {
   pactBroker: 'http://127.0.0.1:3000',
   pactBrokerUsername: 'broker',
   pactBrokerPassword: 'broker',
-  consumerVersion: '2.0.0'
-};
+  consumerVersion: revision = require('child_process').execSync('git rev-parse HEAD').toString().trim()
+}
 
-publisher.publishPacts(opts).then(() => done());
+publisher.publishPacts(opts).then(() => done())
