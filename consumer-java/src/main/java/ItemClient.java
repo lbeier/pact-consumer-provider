0ad1775bad1c55b8d3da400541f3422318d6360f
Lucas Falk Beier <lucas.beier@gmail.com>
2019-04-17 16:25:25 +0200

    Adding java example

--- consumer-java/src/main/java/ItemClient.java
+++ consumer-java/src/main/java/ItemClient.java
@@ -1,8 +1,22 @@
+import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
+
 public class ItemClient {
 
     private String url;
 
     public ItemClient(String url) {
         this.url = url;
+    }
+
+    public String get(String path) {
+        try {
+            return Request.Get(this.url + path)
+                    .addHeader("Content-Type", "application/json")
+                    .execute().returnContent().asString();
+        } catch (IOException e) {
+            return "";
+        }
     }
 }
--- consumer-java/src/test/java/ItemsPactTest.java
+++ consumer-java/src/test/java/ItemsPactTest.java
@@ -1,2 +1,66 @@
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRuleMk2;
+import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.RequestResponsePact;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
 public class ItemsPactTest {
+    private Map<String, String> headers = new HashMap<>() {{
+        put("Content-Type", "application/json");
+    }};
+
+    @Rule
+    public PactProviderRuleMk2 mockProvider
+            = new PactProviderRuleMk2("api-in-go", "localhost", 8080, this);
+
+    @Pact(provider = "api-in-go", consumer = "java_consumer")
+    public RequestResponsePact existingItemFragment(PactDslWithProvider builder) {
+        return builder
+                .given("an item with ID_00001 exists")
+                .uponReceiving("GET request to /items/ID_00001")
+                .path("/items/ID_00001")
+                .method("GET")
+                .willRespondWith()
+                .status(200)
+                .headers(this.headers)
+                .body("{\"sku\": \"ID_00001\", \"title\": \"Nice product\", \"quantityInStock\": 100}")
+                .toPact();
+    }
+
+    @Pact(provider = "api-in-go", consumer = "java_consumer")
+    public RequestResponsePact nonExistinItemFragment(PactDslWithProvider builder) {
+        return builder
+                .given("an item with ID_00002 doesn't exist")
+                .uponReceiving("GET request to /items/ID_00002")
+                .path("/items/ID_00002")
+                .method("GET")
+                .willRespondWith()
+                .status(404)
+                .headers(this.headers)
+                .body("")
+                .toPact();
+    }
+
+    @Test
+    @PactVerification(value = "api-in-go", fragment = "existingItemFragment")
+    public void existingItemFragmentTest() {
+        ItemClient client = new ItemClient(mockProvider.getUrl());
+
+        assertEquals(client.get("/items/ID_00001"), "{\"sku\":\"ID_00001\",\"title\":\"Nice product\",\"quantityInStock\":100}");
+    }
+
+    @Test
+    @PactVerification(value = "api-in-go", fragment = "nonExistinItemFragment")
+    public void nonExistinItemFragmentTest() {
+        ItemClient client = new ItemClient(mockProvider.getUrl());
+
+        assertEquals(client.get("/items/ID_00002"), "");
+    }
 }
